595f4cd3d8d9fd5b237063fb285731b25d7189b3
Abigail Shlomov <anm798@gmail.com>
2021-11-06 19:19:59 +0200

    add server2

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.get("/api/products", function (req, res) {
 });
 
 // /*read product by specific prams: ':id' 8.3 */
+/**hu */
 
 // app.get("/api/products/:id", function (req, res) {
 //   const { id } = req.params;
